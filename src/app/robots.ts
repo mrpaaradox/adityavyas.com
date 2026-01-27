fa6798b3dc7c7825379d9913bbc7f88a2493f1f0
Mr. Paradox <avyas8927@gmail.com>
2026-01-27 15:53:11 +0530

    sitemap urls

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/private/',
     },
-    sitemap: 'http://localhost:3000/sitemap.xml',
+    sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`,
   }
 }
